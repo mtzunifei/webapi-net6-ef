2e9932adba70800b5b7f8abafb0880264f731d6d
Matheus Felix <mtzunifei@gmail.com>
2024-11-23 12:47:08 -0300

    Descompactando arquivo zip

---
+++ dotnet-6-crud-api-master/dotnet-6-crud-api-master/Entities/User.cs
@@ -0,0 +1,16 @@
+namespace WebApi.Entities;
+
+using System.Text.Json.Serialization;
+
+public class User
+{
+    public int id { get; set; }
+    public string Title { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public Role Role { get; set; }
+
+    [JsonIgnore]
+    public string PasswordHash { get; set; }
+}
